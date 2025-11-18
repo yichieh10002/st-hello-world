9235dfe51c5107c3b10e40faf41cb36abee95c61
YC <70553995+yichieh10002@users.noreply.github.com>
2025-11-18 14:42:04 +0800

    Refactor Streamlit app for Uber pickups visualization
    fix:change contain

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,36 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("我的第一個 Streamlit 應用")
-st.header("這是大標題")
-st.subheader("這是中標題")
-st.write("這是普通文字")
-st.write("看出差別了嗎？字的大小不一樣！")
-st.divider()
+st.title('Uber pickups in NYC')
 
-st.title("簡單登入系統")
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-username = st.text_input("帳號")
-password = st.text_input("密碼", type="password")
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 
-if st.button("登入"):
-    if username == "admin" and password == "1234":
-        st.success("登入成功！")
-        st.balloons()
-    else:
-        st.error("帳號或密碼錯誤！")
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
